def3d6180d477cc710592b38126219cb4eb79b8b
luas1 <luvassilva47@gmial.com>
2023-01-13 15:13:19 -0300

    Corrigido o erro da navbar de o usuario acessa varios item por ela a mesma nao retorna nada

--- example-app/resources/views/layouts/main.blade.php
+++ example-app/resources/views/layouts/main.blade.php
@@ -37,6 +37,7 @@
 {{--                    </li>--}}
                     <li class="nav-item">
                         <a class="nav-link" href='/dossies/criar'>Criar Um Dossie</a>
+
                     </li>
 
                     </li>
